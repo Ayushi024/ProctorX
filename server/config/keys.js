a3445ff2c78ba6f83e452442ae081daae604cf06
Ayushi Panwar <ayushi982@gmail.com>
2024-11-26 18:44:27 +0530

    commit

---
+++ server/config/keys.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//     mongoURI: "mongodb+srv://admin:addiction@cluster0.rcsbb.mongodb.net/online-exam-proctoring?retryWrites=true&w=majority",
+//     secretOrKey: "secret",
+//   };
+
+
+
+
+
+  module.exports = {
+    mongoURI: "mongodb+srv://ayushipanwar264:vaFcdHd6TuaRzBM2@cluster-proctor.rocji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-Proctor",
+    secretOrKey: "secret",
+  };
